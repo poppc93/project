659dbc420e1ba1d4cd6ba20a0efd3bb6b7294812
poppc93 <poppc93@skku.edu>
2017-11-03 12:11:17 +0900

    module1최종
    변경된 점
    -25개 -> 100개의 기사를 가져온다
    -파일쓰기에서 유니코드에러 처리
    -하이퍼링크에 한 번만 접근하여 날짜와 본문정보를 동시에 가져옴

--- href.py
+++ href.py
@@ -1,59 +1,62 @@
-# -*- encoding: utf-8 -*-
-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
-import datetime
-import random
-random.seed(datetime.datetime.now())
 
-def getBody(url):
+def getArticle(url):
     html = urlopen(url)
     bs0bj = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
-    body = ''
-    for item in bs0bj.find_all('div', id='articleBodyContents'):
-        body = body + str(item.find_all(text=True))
-    return body
+    article = ['', '']
+    for item in bs0bj.find_all('div', id='articleBodyContents'):#기사의 본문이 있는 태그를 찾음
+        article[0] = article[0] + str(item.find_all(text=True))
+        
+    item = bs0bj.find_all('span', {'class':'t11'})              #기사의 날짜가 있는 태그를 찾음
+    article[1] = article[1] + str(item[0].find_all(text=True))  #첫번째 span태그만을 가져와서 text파일을 가져옴 ->즉 기사의 최초 작성 시기를 가져온다.
+    article[1] = re.sub('[\-:]', ' ', article[1])               # : 과 - 를 공백으로 대체
+    article[1] = re.sub('[\'\[\]]', '', article[1])             # '과 대괄호 제거
 
-def getDate(url):
-    html = urlopen(url)
-    soup = BeautifulSoup(html, 'html.parser')
-    date= ''
-    item = soup.find_all('span', {'class':'t11'})
-    date = date +str(item[0].find_all(text=True))#첫번째 span태그만을 가져와서 text파일을 가져옴 ->즉 기사의 최초 작성 시기를 가져온다.
-    date = re.sub('[\-:]', ' ', date) # : 과 - 를 공백으로 대체
-    date = re.sub('[\'\[\]]', '', date) # '과 대괄호 제거
-    return date
+    return article
+    #article[0]:본문
+    #artilce[1]:날짜 (반환되는 형식은 년 월 일 시 분 초. ex) 2017 11 03 10 18 34)
 
+def clean(body): #기사 본문 내용에서 기사내용과 관련없는 내용을 정리한다
+    spl = re.split('▶', body) # 기사가 끝날 때 '▶'문자와 함께 광고가 나오는데 그 광고의 앞부분만 가져옴
+    cleaned = re.sub('[A-Za-z0-9\._+]+@[A-Za-z]+\.(co.kr|com|net)', '', spl[0]) #기자의 이메일 제거
+    cleaned = re.sub('[\}\{\[\]\/?.,:;|\)*~`!^\-+_<>@\#$%&\\\=\(\'\"]', '', cleaned) #기타 기호 제거
+    cleaned = re.sub('tnt', ' ', cleaned)
+    spl2 = re.split('Callback n n ', cleaned) #기사와 관련없는 문자 제거
+    return spl2[1]
 
-def clean(title):
-    spl = re.split('▶', title)
-    spl2 = re.split('nfunction', spl[0])
-    cleaned = re.sub('[A-Za-z0-9\._+]+@[A-Za-z]+\.(co.kr|com|net)', '', spl2[1]) #이메일제거
-    cleaned = re.sub('[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]', '', cleaned) #기타기호 제거
-    cleaned = re.sub('( n|tnt| n|flashremoveCallback|flash)', '', cleaned)
-    return cleaned
-
-def getArticleLinks(articleURL):
-    html = urlopen("http://news.naver.com/main"+articleURL)
+def getArticleLinks(page):
+    html = urlopen("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101"+page) #기준이 되는 페이지는 네이버뉴스 경제 홈
     bs0bj = BeautifulSoup(html, "html.parser")
     return bs0bj.find("div", {"class":"section_body"}).findAll("a", href=re.compile("^(http://news.naver.com/main/read)"))
+    #기사의 본문으로 연결되는 하이퍼링크 반환
 
+def getPageLink(i):
+    year = "2017"
+    month = "11"
+    date = "02"
+    page = "#&date="+year+"-"+month+"-"+date+" 00:00:00&page="+ str(i)
+    return page #기사 홈에서 페이지링크를 가져온다
 
-links = getArticleLinks("/main.nhn?mode=LSD&mid=shm&sid1=101")
-
-file = open("output.txt", 'a')
-for a in links:
-    hyperlink = a.attrs['href']
-    title = a.attrs['title']
-    body = getBody(hyperlink)
-    date = getDate(hyperlink)
-    print(title)
-    print(date)
-    print(clean(body) + '\n\n')
+file = open("output.txt", 'a', -1,'utf-16')
+for i in range(1, 5):
+    page = getPageLink(i)
+    links = getArticleLinks(page)
+    for a in links:
+        try:
+            hyperlink = a.attrs['href']
+            title = a.attrs['title']
+            article = getArticle(hyperlink)
+            print(title)
+            print(article[1])
+            print(clean(article[0]) + '\n\n')
     
-   # file.write(title+'\n')
-   # file.write(date+'\n')
-    #file.write(clean(body)+'\n')
+            file.write(title+'\n')
+            file.write(article[1]+'\n')
+            file.write(clean(article[0])+'\n')
+        except UnicodeEncodeError as e:
+            print("Unicode Enconde Error")
+       
 file.close()
 
